d41294e5b7a9f16ba81ad1086f816563c1a75df1
Rositsa Zaharieva <71489061+rebrosi@users.noreply.github.com>
2021-02-10 09:54:19 +0200

    Recursion Application task 1 and 3

--- Project2.2/src/uti/RecursionApplication1.java
+++ Project2.2/src/uti/RecursionApplication1.java
@@ -6,6 +6,26 @@ public class RecursionApplication1 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int x = sc.nextInt();
-		
+		if (odd(x) == 1) {
+			System.out.println("The number is odd");
+		} else if (odd(x) == 0) {
+			System.out.println("The number is even");
+		}
+	}
+	public static int odd(int x) {
+		if (x == 1) {
+			return 1;
+		} else if (x == 0) {
+			return 0;
+		}
+		return odd(x - 2);
+	}
+	public static int even(int x) {
+		if (x == 1) {
+			return 1;
+		} else if (x == 0) {
+			return 0;
+		}
+		return even(x - 2);
 	}
 }
--- Project2.2/src/uti/RecursionApplication3.java
+++ Project2.2/src/uti/RecursionApplication3.java
@@ -3,6 +3,7 @@ package uti;
 import java.util.Scanner;
 
 public class RecursionApplication3 {
+	public static int numberOfMoves = 0;
 	public static void main(String[] args) {
 		// Create a Scanner
 		Scanner input = new Scanner(System.in);
@@ -11,6 +12,7 @@ public class RecursionApplication3 {
 		// Find the solution recursively
 		System.out.println("The moves are:");
 		moveDisks(n, 'A', 'B', 'C');
+		System.out.println("The number of moves is " + numberOfMoves);
 	}
 
 	/**
@@ -18,11 +20,14 @@ public class RecursionApplication3 {
 	 * with auxTower
 	 */
 	public static void moveDisks(int n, char fromTower, char toTower, char auxTower) {
-		if (n == 1) // Stopping condition
+		if (n == 1) { // Stopping condition
 			System.out.println("Move disk " + n + " from "+ fromTower + " to " + toTower);
-		else {
+			numberOfMoves++;
+		}else {
+			
 			moveDisks(n - 1, fromTower, auxTower, toTower);
 			System.out.println("Move disk " + n + " from " + fromTower + " to " + toTower);
+			numberOfMoves++;
 			moveDisks(n - 1, auxTower, toTower, fromTower);
 		}
 	}
