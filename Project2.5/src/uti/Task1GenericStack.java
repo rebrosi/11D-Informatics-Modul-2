34c6692e7377f0153b0279369ff3e1de16ac0d4d
Rositsa Zaharieva <71489061+rebrosi@users.noreply.github.com>
2021-02-21 22:06:52 +0200

    Generics part 1 all tasks

--- Project2.5/src/uti/Task1GenericStack.java
+++ Project2.5/src/uti/Task1GenericStack.java
@@ -1,42 +1,38 @@
 package uti;
 
 public class Task1GenericStack<E> {
-	//private java.util.ArrayList <E> list = new java.util.ArrayList<>();
-	private E[] list;
-	private int stackTop;
+	private E[] list = (E[]) new Object[3];
+	private int size = 0;
 	public Task1GenericStack() {
-		stackTop = -1;
+		
 	}
 	
 	public int getSize() {
-		return list.length;
+		return size;
 	}
 
 	public E peek() {
-		return list[stackTop];
+		return list[size - 1];
 	}
 
 	public void push(E o) {
-		if (stackTop + 1 == list.length) {
-			E[] newList;
-			//newList.length = 2 * list.length;
+		if (size >= list.length) {
+			E[] newList = (E[]) new Object[list.length*2];
 			for (int i = 0; i < list.length; i++) {
-				//newList[i] = list[i];
+				newList[i] = list[i];
 			}
 		} else {
-			list[++stackTop] = o;
+			list[++size] = o;
 		}
 	}
 
 	public E pop() {
-		E poped = list[stackTop];
-        list[stackTop] = null;
-        stackTop--;
+		E poped = list[--size];
         return poped;
 	}
 
 	public boolean isEmpty() {
-		return stackTop == -1 ? true:false;
+		return size == 0;
 	}
 
 	@Override
--- Project2.5/src/uti/Task2TestGenericStack.java
+++ Project2.5/src/uti/Task2TestGenericStack.java
@@ -6,22 +6,12 @@ public class Task2TestGenericStack {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		Task2GenericStack<String> stack1 = new Task2GenericStack<>();
-		System.out.println("Please enter 5 strings");
-		stack1.push(sc.next());
-		stack1.push(sc.next());
-		stack1.push(sc.next());
-		stack1.push(sc.next());
-		stack1.push(sc.next());
-		stack1.pop();
-		stack1.pop();
-		stack1.pop();
-		stack1.pop();
-		stack1.pop();
-		
-		Task2GenericStack<Integer> stack2 = new Task2GenericStack<>();
-		stack2.push(1); // autoboxing 1 to new Integer(1)
-		stack2.push(2);
-		stack2.push(3);
+		for(int i = 0; i < 5; i++) {
+			stack1.push(sc.next());
+		}
+		for(int i = 0; i < 5; i++) {
+			System.out.println(stack1.pop());
+		}
 	}
 	
 //	stack1.push("Berlin");
