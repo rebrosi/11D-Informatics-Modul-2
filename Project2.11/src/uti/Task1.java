48fb8989b5b7a396fdffd36df19fe0bdcac94464
Rositsa Zaharieva <71489061+rebrosi@users.noreply.github.com>
2021-03-31 09:47:06 +0300

    Algorithm's efficiency all tasks

--- Project2.11/src/uti/Task1.java
+++ Project2.11/src/uti/Task1.java
@@ -5,12 +5,40 @@ import java.util.Scanner;
 public class Task1 {
 
 	public static void main(String[] args) {
+		//T(n) = O(n)
 		Scanner sc = new Scanner (System.in);
 		System.out.println("Enter two strings");
 		String first = sc.next();
 		String second = sc.next();
-		//O(n)
-		System.out.println(first.contains(second));
+		
+		int index = -1;
+		int count = 0;
+		
+		for (int i = 0; i < first.length(); i++) {
+			if(first.charAt(i) == second.charAt(0) && count == 0) {
+				index = i;
+				count++;
+			} else if (first.charAt(i) == second.charAt(count)) {
+				count++;
+			} else if (count != 0) {
+				count = 0;
+				index = -1;
+				i--;
+			}
+			if (count == second.length()) {
+				break;
+			}
+		}
+		if (count != second.length()) {
+			index = -1;
+			count = 0;
+		}
+		
+		if (index > -1) {
+			System.out.println("matched at index " + index);
+		} else {
+			System.out.println("The second string is not a substring of the first one");
+		}
 
 	}
 
--- Project2.11/src/uti/Task2.java
+++ Project2.11/src/uti/Task2.java
@@ -1,27 +1,33 @@
 package uti;
 
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public abstract class Task2 {
 
 	public static void main(String[] args) {
+			//T(n) = O(n^2)
 	        Scanner input = new Scanner(System.in);
 	        System.out.println("Please enter a string: ");
 	        String userString = input.next();
-
-	        char[] precedent = new char[userString.length()];
-	        precedent[0] = userString.charAt(0);
-	        int len = 1;
-	        for(int i = 1; i < userString.length(); i++){
-	            if(userString.charAt(i) > precedent[len - 1]){
-	                precedent[len] = userString.charAt(i);
-	                len++;
-	            }       
+	        
+	        LinkedList <Character> max = new LinkedList<>();
+	        LinkedList <Character> list = new LinkedList<>();
+	        for(int i = 0; i < userString.length(); i++){
+	        	list.add(userString.charAt(i));
+	        	for (int j = i + 1; j < userString.length(); j++) {
+	        		if (userString.charAt(j) > list.getLast()) {
+	        			list.add(userString.charAt(j));
+	        		}
+	        	}
+	        	if(list.size() > max.size()) {
+	        		max.clear();
+	        		max.addAll(list);
+	        	}
+	        	list.clear();
 	        }
-
-	        for(int i = 0; i < len; i++)
-	            System.out.print(precedent[i]);
-
+	        System.out.println(max.toString());
+	        
 	    }
 	
 }
